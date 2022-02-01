f6c6a76b1cc31cc9fd933961d08393c231b12309
fishZH <jiangzhong91@gmail.com>
2022-02-01 14:34:48 +0800

    init

---
+++ src/test/java/dp/DpApplicationTests.java
@@ -0,0 +1,13 @@
+package dp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
